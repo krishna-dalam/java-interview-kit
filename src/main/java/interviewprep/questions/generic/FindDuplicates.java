039963f188c60ca45be2f75246814cbe3272e245
krishna-dalam <krishnachaithanyadalam@gmail.com>
2020-03-27 17:18:15 +0530

    Adding finding duplicates code

--- src/main/java/interviewprep/questions/generic/FindDuplicates.java
+++ src/main/java/interviewprep/questions/generic/FindDuplicates.java
@@ -1,19 +1,25 @@
 package interviewprep.questions.generic;
 
 
+import java.util.Scanner;
+
 //Given arr with elements from 0 - n-1 where size of arr is n
 public class FindDuplicates {
     public static void main(String args[]) {
-        int numRay[] = {0, 4, 3, 2, 7, 8, 2, 3, 1};
 
-        for (int i = 0; i < numRay.length; i++) {
-            int val = numRay[i] %  numRay.length;
-            System.out.println(val);
-            numRay[numRay[i] %  numRay.length] += numRay.length;
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] input = new int[n];
+        for (int i=0; i<n; i++)
+            input[i] = scanner.nextInt();
+
+
+        for (int i = 0; i < input.length; i++) {
+            input[input[i] %  input.length] += input.length;
         }
         System.out.println("The repeating elements are : ");
-        for (int i = 0; i < numRay.length; i++) {
-            if (numRay[i] >= numRay.length*2) {
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] >= input.length*2) {
                 System.out.println(i + " ");
             }
         }
